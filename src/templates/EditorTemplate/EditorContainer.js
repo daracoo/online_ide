2455e753d3fc841660cef6234c662f80ba714f8c
stefanrikaloski <stefanrikaloski@gmail.com>
2025-02-05 01:36:05 +0100

    added import and export code functionality

--- src/templates/EditorTemplate/EditorContainer.js
+++ src/templates/EditorTemplate/EditorContainer.js
@@ -1,40 +1,114 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEdit, faUpRightAndDownLeftFromCenter, faFileImport, faDownload, faPlay } from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {
+    faEdit,
+    faUpRightAndDownLeftFromCenter,
+    faFileImport,
+    faDownload,
+    faPlay
+} from '@fortawesome/free-solid-svg-icons';
 import './EditorContainer.scss'
-import { Editor } from '@monaco-editor/react';
+import {Editor} from '@monaco-editor/react';
+import {useRef, useState} from "react";
 
 const editorOptions = {
     fontSize: 18,
     wordWrap: 'on'
 }
 
+const fileExtensionMapping = {
+    cpp: 'cpp',
+    javascript: 'js',
+    python: 'py',
+    java: 'java',
+}
+
 export const EditorContainer = () => {
-    return(
+    const [code, setCode] = useState();
+    const [language, setLanguage] = useState("cpp");
+    const [theme, setTheme] = useState('vs-dark');
+    const codeRef = useRef()
+
+
+    const onChangeCode = (newCode) => {
+        codeRef.current = newCode;
+    }
+
+    const importCode = (event) => {
+        const file = event.target.files[0];
+        const fileType = file.type.includes("text");
+        if (fileType) {
+            const fileReader = new FileReader();
+            fileReader.readAsText(file)
+            fileReader.onload = function (value) {
+                const importedCode = value.target.result;
+                setCode(importedCode)
+                codeRef.current = importedCode;
+            }
+        } else {
+            alert("Please choose a program file!");
+        }
+    }
+
+    const exportCode = () => {
+        const codeValue = codeRef.current?.trim();
+
+        if (!codeValue) {
+            alert("Please type some code in the editor before exporting!")
+        }
+            // креирам blob за инстант file во меморија
+            const codeBlob = new Blob([codeValue], {type: "text/plain"})
+
+            // линк за симнување со blob дата
+            const downloadUrl = URL.createObjectURL(codeBlob)
+
+            // креирам линк за симнување на фајлот :DDDD
+            const link = document.createElement("a");
+            link.href = downloadUrl;
+            // const fileExtension =
+            link.download = `code.${fileExtensionMapping[language]}`
+            link.click()
+
+
+
+    }
+
+    const onChangeLanguage = (e) => {
+        setLanguage(e.target.value)
+    }
+
+    const onChangeTheme = (e) => {
+        setTheme(e.target.value);
+    }
+
+    return (
         <div className="root-editor-container">
             <div className='editor-header'>
                 <div className='editor-left-container'>
                     <b className='title'>{"Title"}</b>
-                    <span><FontAwesomeIcon icon={ faEdit }/></span>
+                    <span><FontAwesomeIcon icon={faEdit}/></span>
                     <button>Save Code</button>
                 </div>
                 <div className='editor-right-container'>
-                   <select>
+                    <select onChange={onChangeLanguage} value={language}>
                         <option value="cpp">cpp</option>
                         <option value="javascript">javascript</option>
                         <option value="java">java</option>
                         <option value="python">python</option>
-                   </select>
-                   <select>
+                    </select>
+                    <select onChange={onChangeTheme} value={theme}>
                         <option value="vs-dark">vs-dark</option>
                         <option value="vs-light">vs-light</option>
-                   </select>
+                    </select>
                 </div>
             </div>
             <div className='editor-body'>
                 <Editor height={"100%"}
-                language={"javascript"}
-                options={editorOptions}
-                theme={'vs-dark'}/>
+                        language={language}
+                        options={editorOptions}
+                        theme={theme}
+                        onChange={onChangeCode}
+                        value={code}
+                />
             </div>
             <div className='editor-footer'>
                 <button className='btn'>
@@ -45,13 +119,13 @@ export const EditorContainer = () => {
                     <span><span><FontAwesomeIcon icon={faFileImport}/></span></span>
                     <span>Import Code</span>
                 </label>
-                <input type='file' id='import-code' style={{display: 'none'}}></input>
-                <button className='btn'>
+                <input type='file' id='import-code' style={{display: 'none'}} onChange={importCode}></input>
+                <button className='btn' onClick={exportCode}>
                     <span><FontAwesomeIcon icon={faDownload}/></span>
                     <span>Export Code</span>
                 </button>
                 <button className='btn'>
-                    <span><FontAwesomeIcon icon={ faPlay }/></span>
+                    <span><FontAwesomeIcon icon={faPlay}/></span>
                     <span>Run Code</span>
                 </button>
             </div>
--- src/templates/EditorTemplate/editor.js
+++ src/templates/EditorTemplate/editor.js
@@ -1,12 +1,49 @@
-import { useParams } from "react-router-dom";
+import {useParams} from "react-router-dom";
 import "./style.scss";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFileImport, faDownload } from '@fortawesome/free-solid-svg-icons';
-import { EditorContainer } from "./EditorContainer";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faFileImport, faDownload} from '@fortawesome/free-solid-svg-icons';
+import {EditorContainer} from "./EditorContainer";
+import {useState} from "react";
 
 export const EditorTemplate = () => {
     const params = useParams();
     console.log(params);
+
+
+    
+    const [input, setInput] = useState('')
+    const [output, setOutput] = useState('')
+
+    const importInput = (e) => {
+        const file = e.target.files[0]
+        const fileType = file.type.includes("text")
+        if (fileType) {
+            const fileReader = new FileReader()
+            fileReader.readAsText(file)
+            fileReader.onload = (e) => {
+                setInput(e.target.result)
+
+            }
+        } else {
+            alert("Please choose a program file!");
+        }
+    }
+
+    const exportOutput = () => {
+        const outputValue = output.trim();
+        if(!outputValue){
+            alert("The output is empty!");
+        } else {
+            const blob = new Blob([outputValue], {type: "text/plain"})
+            const url = URL.createObjectURL(blob)
+            const link = document.createElement("a");
+            link.href = url;
+            link.download = "output.txt";
+            link.click()
+        }
+    }
+
+
     return (
         <div className="page-container">
             <div className="header-container">
@@ -14,7 +51,7 @@ export const EditorTemplate = () => {
             </div>
             <div className="content-container">
                 <div className="editor-container">
-                    <EditorContainer/>
+                    <EditorContainer fileId={fileId}/>
                 </div>
                 <div className="input-output-container">
                     <div className="input-header">
@@ -23,19 +60,19 @@ export const EditorTemplate = () => {
                             <span><FontAwesomeIcon icon={faFileImport}/></span>
                             <b className="">Import Input</b>
                         </label>
-                        <input type="file" id="input" style={{display: 'none'}}></input>
+                        <input type="file" id="input" style={{display: 'none'}} onChange={importInput}></input>
                     </div>
-                    <textarea></textarea>
+                    <textarea value={input} onChange={ (e) => setInput(e.target.value)}> </textarea>
                 </div>
                 <div className="input-output-container">
                     <div className="input-header">
                         <b>Output: </b>
-                        <button className="icon-container">
+                        <button className="icon-container" onClick={exportOutput}>
                             <span><FontAwesomeIcon icon={faDownload}/></span>
                             <b>Export Output</b>
                         </button>
                     </div>
-                    <textarea readOnly></textarea>
+                    <textarea readOnly value={output} onChange={(e) => setOutput(e.target.value)}></textarea>
                 </div>
             </div>
         </div>
